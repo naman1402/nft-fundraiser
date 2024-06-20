21b8a67464b6924dce8d41d210f4376a444d9c31
naman mohnani <mohnaninaman@gmail.com>
2024-06-21 00:43:26 +0530

    testing ...

--- contracts/FundRaiser.sol
+++ contracts/FundRaiser.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract FundRaiser is ERC721URIStorage{
 
     error FundRaiser_NotOwnerOfNFT();
     error FundRaiser_Completed();
+    error FundRaiser_ZeroDonation();
+    error FundRaiser_CampaignNotExists();
+    error FundRaiser_OverPaid();
+    error FundRaiser_NotEnoughRaised();
+    error FundRaiser_TransferFailed();
 
     event StartCampaign(address indexed owner, uint256 indexed id, uint256 reqAmount, uint256 when);
     event ExtendCampaign(address indexed owner, uint256 indexed id,  uint256 additionalAmount);
+    event Donation(address indexed donationFrom, uint256 indexed id, uint256 amount);
+    event Withdraw(address indexed withdrawTo, uint256 indexed id, uint256 amount);
+    event EndCampaign(uint256 indexed id);
+    
 
     struct C {
         uint256 amtRaised;
@@ -52,14 +61,57 @@ contract FundRaiser is ERC721URIStorage{
         emit ExtendCampaign(msg.sender, id, additionalAmount);
     }
 
+    function donateToCampaign(uint256 id) payable external campaignCompleted(id) {
+        if (msg.value == 0) {
+            revert FundRaiser_ZeroDonation();
+        }
+        if (ownerOf(id)==address(0)) {
+            revert FundRaiser_CampaignNotExists();
+        }
+        C storage campaign = s_IdToCampaign[id];
 
+        if(campaign.amtRaised + msg.value > campaign.reqAmt) {
+            revert FundRaiser_OverPaid();
+        }
+        campaign.amtRaised += msg.value;
+        emit Donation(msg.sender, id,  msg.value);
+    }
 
+    function withdrawFromCampaign(uint256 id, uint256 amount) external onlyNFTOwner(id) campaignCompleted(id) {
+        C storage campaign = s_IdToCampaign[id];
+        if(amount > campaign.amtRaised) {
+            revert FundRaiser_NotEnoughRaised();
+        }
+        campaign.amtRaised -= amount;
+        campaign.reqAmt -= amount;
 
+        (bool success, ) = msg.sender.call{value:amount}("");
+        if(!success) {
+            revert FundRaiser_TransferFailed();
+        }
 
+        emit Withdraw(msg.sender, id, amount);
+    } 
 
+    function endCampaign(uint256 id) external onlyNFTOwner(id) campaignCompleted(id) {
+        s_IdToCampaign[id].completed = true;
+        (bool success, ) = msg.sender.call{value:s_IdToCampaign[id].amtRaised}("");
+        if (!success) {
+            revert FundRaiser_TransferFailed();
+        }
 
+        emit EndCampaign(id);
+    }
 
-    
+    // getter function
+    function getCampaign(uint id) external view returns (C memory) {
+        return s_IdToCampaign[id];
+    }
+
+    function getLatestId() external view returns (uint256) {
+        return s_id;
+    }
+ 
 }
 
 
---
+++ test/Fundraiser.test.ts
@@ -0,0 +1,86 @@
+import { ethers, hardhatArguments } from "hardhat"
+import { FundRaiser, FundRaiser__factory } from "../typechain-types"
+import { parseEther } from "ethers"
+import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers"
+import { assert } from "console"
+import { deployContract } from "@nomicfoundation/hardhat-ethers/types"
+import { expect } from "chai"
+import exp from "constants"
+
+describe("Fundraiser Test", () => {
+    let tokenURI = "www.google.com"
+    let requiredAmt = parseEther("10")
+    let fundraiser: FundRaiser
+    let deployer: SignerWithAddress
+    let user: SignerWithAddress
+    let accounts: SignerWithAddress[]
+    let campaign: FundRaiser.CStructOutput
+
+    beforeEach(async() => {
+        ;[deployer, user, ...accounts] = await ethers.getSigners()
+        
+        fundraiser = await ethers.getContractAt("FundRaiser", deployer)
+    })
+
+    it("Constructor", async () => {
+        assert(fundraiser.name(), "FundRaiser")
+        assert( fundraiser.symbol(), "FRC")
+    })
+
+    it("get latest token id", async() => {
+        assert( fundraiser.getLatestId(), "0")
+    })
+
+
+    describe("startCampaign", () => {
+        beforeEach(async () => {
+            await fundraiser.startCampaign(tokenURI, requiredAmt)
+        })
+
+        it("check ownership", async () => {
+            assert(await fundraiser.ownerOf(1), deployer.address)
+            // console.log(fundraiser.ownerOf(1))
+            // console.log(deployer.address) //0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+        })
+
+        it("check token uri", async () => {
+            assert(await fundraiser.tokenURI(1), tokenURI)
+        })
+
+        it("check required amount", async () => {
+            campaign = await fundraiser.getCampaign(1)
+            assert(campaign.reqAmt.toString(), requiredAmt.toString())
+        })
+
+        it("StartCampaign event", async () => {
+            await expect(fundraiser.startCampaign(tokenURI, requiredAmt))
+                .to.emit(fundraiser, "StartCampaign")
+                .withArgs(deployer.address, 2, requiredAmt)
+        })
+    })
+
+
+    describe("extendCampaign", () => {
+        let extendAmount = parseEther("5")
+        beforeEach(async () => {
+            await fundraiser.startCampaign(tokenURI, requiredAmt)
+        })
+        it("check onlyNFTOwner modifier", async () => {
+            await expect(fundraiser.connect(user).extendCampaign(1, extendAmount)).to.be.revertedWithCustomError(fundraiser, "FundRaiser_NotOwnerOfNFT")
+        })
+        it("check notCompleted modifier", async () => {
+            let tx = await fundraiser.endCampaign(1)
+            await tx.wait()
+            await expect(fundraiser.extendCampaign(1, extendAmount)).to.be.revertedWithCustomError(fundraiser, "FundRaiser_Completed")
+        })
+        it("ExtendCampaign event", async () => {
+            await expect(fundraiser.extendCampaign(1, extendAmount)).to.emit(fundraiser, "ExtendCampaign").withArgs(deployer.address, 1, extendAmount)
+        })
+    })
+
+    describe("donateCampaign", () => {
+        
+    })
+})
+
+
